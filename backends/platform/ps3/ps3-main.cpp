a43b454d28bf225c0c9f295d744ee2bd3a19cc9d
Martin Büchler <lousy@lousy.at>
2010-11-09 17:44:46 +0100

    fixed the build - PSL1GHT was missing a funtion :P

--- backends/platform/ps3/ps3-main.cpp
+++ backends/platform/ps3/ps3-main.cpp
@@ -15,14 +15,8 @@ int main(int argc, const char* argv[])
 {
 	printf("INIT!\n");
 	g_system = OSystem_PS3_create();
-	printf("1\n");
-
 	assert(g_system);
 	
-	printf("1\n");
-
-	lv2TimerUSleep(10*1000*1000);
-
 	scummvm_main(argc, argv);
 
 	printf("SHUTDOWN!\n");
